46506a7eeae15ec54ffc5c88774fdcc21c39efdd
vominhtrungpro <68464213+vominhtrungpro@users.noreply.github.com>
2022-06-08 05:26:34 +0700

    Add files via upload

---
+++ DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,37 @@
+﻿
+public interface IHorn
+{
+    void Beep();
+}
+class Horn : IHorn {
+
+    public void Beep() => Console.WriteLine("Beep...beep...beep");
+}
+class Car {
+    private IHorn _horn;
+    public Car(IHorn horn) //Constructor Injection
+    {
+        this._horn = horn;
+    }
+
+    //public IHorn Horn //Property Injection
+    //{
+    //    set { this._horn = value; }
+    //}
+
+    public IHorn horn;
+    public void CarHorn(IHorn _horn) {
+        horn = _horn;
+        this._horn.Beep();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Horn h = new Horn();
+        Car car = new Car(h);
+        //car.Horn = h;
+        car.CarHorn(h);
+    }
+}
